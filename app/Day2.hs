e412e4c55656baeb64e7a4de415d9b3a25562c4d
fkrause98 <fkrausear@gmail.com>
2024-12-02 13:45:55 -0300

    solve day 2

--- app/Day2.hs
+++ app/Day2.hs
@@ -1,6 +1,6 @@
 module Main where
 import Data.List
-import Data.Bifunctor
+import Data.Bifunctor()
 
 part1 :: IO ()
 part1 = do
@@ -9,39 +9,47 @@ part1 = do
   let levelsInt = map (\lst -> (map (read) lst) :: [Integer]) levels
   let safeLevels = map isLevelSafe levelsInt
   let asNums = foldl (\total isSafeLevel -> if isSafeLevel then 1 + total else total) 0 safeLevels
-  print asNums
+  putStrLn $ "Part 1: " ++ show asNums
 
+
+part2 :: IO ()
+part2 = do
+  input <- readFile "./input/day2.txt"
+  let levels = map words (lines input)
+  let levelsInt = map (\lst -> (map (read) lst) :: [Integer]) levels
+  let safeLevels = map isLevelSafe2 levelsInt
+  let asNums = foldl (\total isSafeLevel -> if isSafeLevel then 1 + total else total) 0 safeLevels
+  putStrLn $ "Part 2: " ++ show asNums
 
 
 allIncreasing :: [Integer] -> Bool
 allIncreasing (x:y:ys) =
   x > y && allIncreasing (y:ys)
-allIncreasing xs = True
+allIncreasing _ = True
 
 allDecreasing :: [Integer] -> Bool
 allDecreasing (x:y:ys) =
   x < y && allDecreasing (y:ys)
-allDecreasing xs = True
+allDecreasing _ = True
 
 diffsInRange :: [Integer] -> Bool
 diffsInRange (x:y:ys) =
   (abs (x-y) >= 1) && (abs (x-y) <= 3) && diffsInRange (y:ys)
-diffsInRange xs = True
+diffsInRange _ = True
 
 isLevelSafe :: [Integer] -> Bool
 isLevelSafe lst = (allIncreasing lst || allDecreasing lst) && diffsInRange lst
 
--- part2 :: IO ()
--- part2 = do
---   input <- readFile "./input/day1.txt"
---   let (list1, list2) = createAndSortLists $ inputAsTuples input
---   let appearances = map (`calcAppearances` list2)  list1
---   let scores = zipWith (*) list1 appearances
---   putStrLn $ "Part 2 solution: " ++ show (sum scores)
+isLevelSafe2 :: [Integer] -> Bool
+isLevelSafe2 =
+  foldl (\isSafe acum -> isSafe || acum) False
+  . map isLevelSafe
+  . generateVariations
+
+generateVariations :: [Integer] -> [[Integer]]
+generateVariations lst = zipWith (++) (inits lst) (tail (tails lst))
 
 main :: IO ()
 main = do
   part1
-  -- part2
-
-
+  part2
