12aab0905c0cb7e8b69aea76da1574c46014a251
fkrause98 <fkrausear@gmail.com>
2024-12-02 02:05:27 -0300

    improve day 1

--- app/Main.hs
+++ app/Main.hs
@@ -7,20 +7,24 @@ part1 = do
   input <- readFile "./input/day1.txt"
   let (list1, list2) = createAndSortLists $ inputAsTuples input
   let distances = calcDistances list1 list2
-  print $ sum distances
+  putStrLn $ "Part 1 solution: " ++ show (sum distances)
 
-main :: IO ()
-main = do
+part2 :: IO ()
+part2 = do
   input <- readFile "./input/day1.txt"
   let (list1, list2) = createAndSortLists $ inputAsTuples input
   let appearances = map (`calcAppearances` list2)  list1
   let scores = zipWith (*) list1 appearances
-  print (sum scores)
+  putStrLn $ "Part 2 solution: " ++ show (sum scores)
+
+main :: IO ()
+main = do
+  part1
+  part2
 
 
 calcAppearances :: Integer -> [Integer] -> Integer
-calcAppearances _ [] = 0
-calcAppearances x (y:ys) = if x == y then 1 + calcAppearances x ys else calcAppearances x ys
+calcAppearances x = fromIntegral . length . filter (== x)
 
 calcDistances :: [Integer] -> [Integer] -> [Integer]
 calcDistances = zipWith (\x y -> abs (x - y))
@@ -30,8 +34,7 @@ createAndSortLists tuplesInput = bimap sort sort (unzip tuplesInput)
 
 inputAsTuples :: String -> [(Integer, Integer)]
 inputAsTuples input = map lineToTuple (lines input)
-
-lineToTuple :: String -> (Integer, Integer)
-lineToTuple line = case words line of
-  [elem1, elem2] -> (read elem1, read elem2)
-  _ -> error "Wrong input"
+  where
+    lineToTuple line = case words line of
+      [elem1, elem2] -> (read elem1, read elem2)
+      _ -> error "Wrong input"
