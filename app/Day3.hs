8ee22b4fdcdca705acdcd62d5c637ef6066e4761
fkrause98 <fkrausear@gmail.com>
2024-12-03 22:51:09 -0300

    day 3 finished

--- app/Day3.hs
+++ app/Day3.hs
@@ -1,9 +1,10 @@
 module Main where
 import Data.Bifunctor()
-import Text.Megaparsec
+import Text.Megaparsec hiding (State)
 import Text.Megaparsec.Char
 import Text.Megaparsec.Char.Lexer
 import Data.Void
+import Control.Monad.State
 type Parser = Parsec Void String
 
 leftParen :: Parser Char
@@ -12,20 +13,36 @@ rightParen :: Parser Char
 rightParen = char ')'
 comma :: Parser Char
 comma = char ','
-mul :: Parser Char
-mul = do
-  _ <- char 'm'
-  _ <- char 'u'
-  char 'l'
 mulInstruction :: Parser (Integer, Integer)
 mulInstruction = do
-  _ <- mul
+  _ <- string "mul"
   _ <- leftParen
   x <- decimal
   _ <- comma
   y <- decimal
   _ <- rightParen
   return (x, y)
+
+_do :: Parser Instruction
+_do = do
+  _ <- char 'd'
+  _ <- char 'o'
+  _ <- leftParen
+  _ <- rightParen
+  return Do
+
+dont :: Parser Instruction
+dont = do
+  _ <- string "don't"
+  _ <- leftParen
+  _ <- rightParen
+  return Dont
+
+instruction :: Parser Instruction
+instruction = try (_do) <|> try (dont) <|> (Mul <$> mulInstruction)
+
+
+data Instruction = Mul (Integer, Integer) | Dont | Do deriving Show
 
 extract :: Parser a -> Parser [a]
 extract p = do (:) <$> try p <*> extract p
@@ -46,16 +63,31 @@ part1 = do
     Right results -> print $ "Part 1 result: " ++ show (getResult results)
 
 
--- part2 :: IO ()
--- part2 = do
---   input <- readFile "./input/day2.txt"
---   let levels = map words (lines input)
---   let levelsInt = map (\lst -> (map (read) lst) :: [Integer]) levels
---   let safeLevels = map isLevelSafe2 levelsInt
---   let asNums = foldl (\total isSafeLevel -> if isSafeLevel then 1 + total else total) 0 safeLevels
---   putStrLn $ "Part 2: " ++ show asNums
+type MulState = (Bool, Integer)
 
+dosAndDonts :: [Instruction] -> State MulState Integer
+dosAndDonts [] = do
+  (_, totalSum) <- get
+  return totalSum
+dosAndDonts (x:xs) = do
+  (isEnabled, totalSum) <- get
+  case x of
+    Mul (a, b) | isEnabled -> put (isEnabled, totalSum + a*b)
+    Mul _ -> put (isEnabled, totalSum)
+    Dont -> put (False, totalSum)
+    Do -> put (True, totalSum)
+  dosAndDonts xs
+
+part2 :: IO ()
+part2 = do
+  input <- readFile "./input/day3.txt"
+  let matches = parse (extract instruction) "" input
+  case matches of
+    Left err -> print $ errorBundlePretty err
+    Right results -> do
+      let startState = (True, 0)
+      print $ evalState (dosAndDonts results) startState
 main :: IO ()
 main = do
   part1
-  -- part2
+  part2
